88d0f3b15282e66f2e2e0a93bf0120d48ce7fc64
root <root@localhost.localdomain>
2018-07-01 06:29:35 +0700

    Platformer Game

---
+++ Assets/Scripts/QuitOrNext.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class QuitOrNext : MonoBehaviour
+{
+	public string NameScene = "Level2";
+	private void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		Debug.Log(collision.tag);
+		if (collision.tag.Equals("Player"))
+		{
+			SceneManager.LoadScene(NameScene);
+
+		}
+	}
+}
